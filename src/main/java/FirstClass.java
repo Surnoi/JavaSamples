29cf7cb9cfccbfb2b7a9ccc5af2366fe7462bfb2
Siraj537 <sirajp71@gmail.com>
2023-03-01 22:31:30 +0530

    added third sout

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -3,6 +3,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hiiiii");
         System.out.println("hello");
+ 	System.out.println("How are you?");
     }
 
 }
