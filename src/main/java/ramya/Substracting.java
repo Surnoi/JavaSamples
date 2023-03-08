8aa97f5fa4f652995cf6d46040bbc47cf714b388
NookaRamya <nramya1012@gmail.com>
2023-03-08 17:18:25 +0530

    New topics related samples

---
+++ src/main/java/ramya/Substracting.java
@@ -0,0 +1,14 @@
+package ramya;
+
+public class Substracting {
+    public static void main(String[] args) {
+        //initializing variables
+        int num1 = 20, num2 = 10, sum = 0;
+        //displaying num1 and nums
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+        //substracting num1 and num2
+        int sub = num1 - num2;
+        System.out.println("substraction = " + sub);
+    }
+}
