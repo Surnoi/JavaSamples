fe750c0f5f8fdb3a059318fa536216ea4880eae2
NookaRamya <nramya1012@gmail.com>
2023-03-08 20:08:06 +0530

    New message

---
+++ src/main/java/ramya/Accept.java
@@ -0,0 +1,17 @@
+package ramya;
+import java.util.Scanner;
+
+public class Accept {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int n = in.nextInt();
+        if (n % 2 == 0) {
+            System.out.println(1);
+
+
+    }
+}
+}
+
+
--- src/main/java/ramya/Average.java
+++ src/main/java/ramya/Average.java
@@ -1,2 +1,16 @@
-package ramya;public class Average {
+package ramya;
+
+import java.util.Scanner;
+
+public class Average {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        float a, b, avg;
+        System.out.println("\n Enter 3 nums : ");
+        a = s. nextFloat();
+        b = s. nextFloat();
+        float c = s.nextFloat();
+        avg = (a + b + c) / 3;
+        System.out.println("\n Average =" + avg);
+    }
 }
