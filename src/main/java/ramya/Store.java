00c80dcb4016181764dd32d72a3c7b012733d430
NookaRamya <nramya1012@gmail.com>
2023-03-08 21:57:33 +0530

    new messages

---
+++ src/main/java/ramya/Store.java
@@ -0,0 +1,18 @@
+package ramya;
+
+public class Store {
+
+    public static void main(String[] args)
+    {
+        int[] myarray;
+        myarray = new int[5];
+        myarray[0] = 10;
+        System.out.println("myarray[0] = " + myarray[0]);
+        System.out.println("myarray[1] = " + myarray[1]);
+        int [] oddArray = {1,3,5,7};
+        System.out.println("oddArray[0] = " + oddArray[0]);
+        System.out.println("oddArray[1] = " + oddArray[1]);
+        System.out.println("oddArray[2] = " + oddArray[2]);
+        System.out.println("oddArray[3] = " + oddArray[3]);
+    }
+}
