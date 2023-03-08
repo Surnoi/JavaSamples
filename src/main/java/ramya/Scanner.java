1f965202cb9a4df0237bec7e71757b9452c9052e
NookaRamya <nramya1012@gmail.com>
2023-03-08 22:35:52 +0530

    New messages

---
+++ src/main/java/ramya/Scanner.java
@@ -0,0 +1,12 @@
+package ramya;
+
+public class Scanner {
+
+        public static void main(String args[]) {
+            System.out.println("Name: Sneha Nayak");
+            System.out.println("age: 25");
+            System.out.println("city: Hyderabad");
+            System.out.println("pincode: 516387");
+        }
+    }
+
