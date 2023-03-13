118ac6c974d34820f53b90151734e0e4e0d39c08
NookaRamya <nramya1012@gmail.com>
2023-03-13 14:05:35 +0530

    New messages

---
+++ src/main/java/practise/Bike.java
@@ -0,0 +1,23 @@
+package practise;
+
+public class Bike {
+    public static void main(String[] args) {
+
+    }
+    public void animalSound() {
+
+        System.out.println("The animal makes a sound");
+    }
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("The pig says: wee wee");
+    }
+}
+
+class Dog extends Animal {
+    public void animalSound() {
+        System.out.println("The dog says: bow wow");
+    }
+}
