20765b0d19acf8ce986a6d2793b3bd462aafecc7
NookaRamya <nramya1012@gmail.com>
2023-03-13 12:54:54 +0530

    New messages

---
+++ src/main/java/practise/Constructor.java
@@ -0,0 +1,20 @@
+package practise;
+
+import com.sun.tools.javac.Main;
+
+public class Constructor {
+    private static String[] args;
+    static int x;
+    public static void main(String[] args) {
+        int y = 0;
+        x= y;
+    }
+
+    {
+        for (String s : Constructor.args = args) {
+            
+        }
+
+        System.out.println("myObj. x");
+    }
+}
